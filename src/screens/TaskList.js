ae39d5ada3e8545db39ce0d7322eb2c55f3b79d7
valdemir.pedro <valdemirassispedro@gmail.com>
2025-02-19 20:14:14 -0300

    cria a estrutura da tela TaskList

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
-
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
 
 export default function TaskList() {
     return(
-        <View>
-            <Text>TaskList</Text>
+        <View style={styles.container}>
+            <ImageBackground source={{}} style={styles.background}>
+
+            </ImageBackground>
+            <View style={styles.taskList}>
+                <Text>Task #01</Text>
+            </View>
+
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3,
+        backgroundColor: '#cfb698'
+    },
+    taskList: {
+        flex: 7,
+        backgroundColor: '#ff5d57'
+    }
+})
