009b3d3551a6abe5afd6d7de9986e4bc2c74a0ea
valdemir.pedro <valdemirassispedro@gmail.com>
2025-03-10 19:28:38 -0300

    cria o botão adicionar task

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,21 +1,38 @@
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity } from "react-native"
+
+import moment from 'moment-timezone'
+import 'moment/locale/pt-br'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import todayImage from '../../assets/imgs/today.jpg'
 
 export default function TaskList() {
+
+    const today = moment().tz("America/Sao_Paulo")
+        .locale("pt-br").format('ddd, D [de] MMMM')
+
     return(
         <View style={styles.container}>
             <ImageBackground source={todayImage} style={styles.background}>
 
                 <View style={styles.titleBar}>
                     <Text style={styles.title}>Hoje</Text>
-                    <Text style={styles.subtitle}>24 de Fevereiro</Text>
+                    <Text style={styles.subtitle}>{today}</Text>
                 </View>
 
             </ImageBackground>
             <View style={styles.taskList}>
                 <Text>Task #01</Text>
             </View>
+
+            <TouchableOpacity style={styles.addButton}
+                activeOpacity={0.7}
+                onPress={() => console.warn("+")}>
+                
+                <Icon name="plus" size={20} color={"#000"} />
+
+            </TouchableOpacity>
 
         </View>
     )
