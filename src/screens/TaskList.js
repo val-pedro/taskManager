80e38910daa012abd3665359c894c815a20058c5
valdemir.pedro <valdemirassispedro@gmail.com>
2025-03-12 20:26:34 -0300

    cria o componente Task

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -6,6 +6,7 @@ import 'moment/locale/pt-br'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import todayImage from '../../assets/imgs/today.jpg'
+import Task from "../components/Task"
 
 export default function TaskList() {
 
@@ -29,7 +30,7 @@ export default function TaskList() {
 
             </ImageBackground>
             <View style={styles.taskList}>
-                <Text>Task #01</Text>
+                <Task />
             </View>
 
             <TouchableOpacity style={styles.addButton}
